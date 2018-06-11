01e3c35b5d6fe1f66512879db90f7be8fc7fe1ed
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-06-11 03:44:49 +0000

    automatically backed up by learn

--- src/components/app.js
+++ src/components/app.js
@@ -3,7 +3,9 @@ import UserBlackjack from 'user_blackjack'
 import AIBlackjack from 'ai_blackjack'
 
 export default class App extends Component{
-
+  constructor(props){
+    super(props)
+  }
   render(){
     return(
       <UserBlackjack />
